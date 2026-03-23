0bdbab15bf85ceee346e88196d2425173aaefee0
lpacheco22 <lpacheco@byupathway.edu>
2026-03-23 15:15:15 -0500

    W03 Scripture-Memo

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+
+        Scripture scripture = new Scripture(reference,
+            "Trust in the Lord with all thine heart and lean not unto thine own understanding");
+
+        string input = "";
+
+        while (input != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+
+            Console.WriteLine("\nPress Enter or type 'quit':");
+            input = Console.ReadLine();
+
+            if (input != "quit")
+            {
+                scripture.HideRandomWords(3);
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplayText());
+        Console.WriteLine("\nProgram ended.");
     }
 }
