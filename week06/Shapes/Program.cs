883f3c3c01ac2afa9682e6d32e3cf548a5cba7f3
lpacheco22 <lpacheco@byupathway.edu>
2026-04-08 15:26:27 -0500

    W06 Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Notice that the list is a list of "Shape" objects. That means
+        // you can put any Shape objects in there, and also, any object where
+        // the class inherits from Shape
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
